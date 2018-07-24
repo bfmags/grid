503fab04580ec239a50a82a5351192a72e08fff4
Akash A <akash1810@gmail.com>
2018-07-24 12:47:11 +0100

    allow multiple images to be added to album via multi-selection

--- kahuna/public/js/components/gr-album/gr-album.js
+++ kahuna/public/js/components/gr-album/gr-album.js
@@ -13,13 +13,37 @@ export const album = angular.module('gr.album', [
 ]);
 
 album.controller('GrAlbumCtrl', [
+    '$scope',
     'imageService',
     'mediaApi',
     'imageAccessor',
     'albumService',
 
-    function(imageService, mediaApi, imageAccessor, albumService) {
+    function($scope, imageService, mediaApi, imageAccessor, albumService) {
         const ctrl = this;
+
+        function refresh() {
+            const apiAlbums = ctrl.images.map(image => imageAccessor.getAlbum(image));
+
+            const albums = apiAlbums.reduce((acc, album) => {
+                return album.data ? [...acc, album.data] : acc;
+            }, []);
+
+            ctrl.hasAlbum = albums.length > 0;
+
+            if (!ctrl.hasAlbum) {
+                ctrl.hasMultipleAlbums = false;
+            } else {
+                const allImagesHaveAlbum = albums.length === Array.from(ctrl.images).length;
+                const uniqueAlbumTitles = new Set(albums.map(album => album.title));
+
+                ctrl.hasSingleAlbum = uniqueAlbumTitles.size === 1 && allImagesHaveAlbum;
+
+                if (ctrl.hasSingleAlbum) {
+                    ctrl.album = albums[0];
+                }
+            }
+        }
 
         ctrl.search = (q) => {
             return mediaApi.metadataSearch('album', { q })
@@ -31,28 +55,16 @@ album.controller('GrAlbumCtrl', [
                 return ctrl.remove();
             }
 
-            return albumService.add({ image: ctrl.image, data: { title }})
-                .then(updatedImage => {
-                    ctrl.image = updatedImage;
-                    ctrl.refresh();
-                });
+            return albumService.batchAdd({ images: ctrl.images, data: { title } })
+                .then(() => refresh());
         };
 
         ctrl.remove = () => {
-            return albumService.remove({ image: ctrl.image })
-                .then(updatedImage => {
-                    ctrl.image = updatedImage;
-                    ctrl.refresh();
-                });
+            return albumService.batchRemove({ images: ctrl.images })
+                .then(() => refresh());
         };
 
-        ctrl.refresh = () => {
-            const apiAlbum = imageAccessor.getAlbum(ctrl.image);
-            ctrl.hasAlbum = !!apiAlbum.data;
-            ctrl.album = ctrl.hasAlbum && apiAlbum.data;
-        };
-
-        ctrl.refresh();
+        $scope.$watchCollection(() => Array.from(ctrl.images), refresh);
     }
 ]);
 
@@ -60,7 +72,7 @@ album.directive('grAlbum', [function() {
     return {
         restrict: 'E',
         scope: {
-            image: '='
+            images: '='
         },
         controller: 'GrAlbumCtrl',
         controllerAs: 'ctrl',
